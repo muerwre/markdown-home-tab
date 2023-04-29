6332e27cdc3d13388413b1a94a19027e8779d849
Fedor Katurov <gotham48@gmail.com>
2023-04-29 22:20:10 +0600

    added bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite";
+import { PluginOption, defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import svgr from "vite-plugin-svgr";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ exportAsDefault: true })],
+  plugins: [
+    react(),
+    svgr({ exportAsDefault: true }),
+    visualizer({
+      template: "treemap", // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "analice.html",
+    }) as PluginOption,
+  ],
   resolve: {
     alias: [{ find: "~", replacement: path.resolve("src") }],
   },
