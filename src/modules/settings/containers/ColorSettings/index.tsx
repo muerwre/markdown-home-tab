d71a3958687d7c75d74f40ab97ca61904d6b9f60
Fedor Katurov <gotham48@gmail.com>
2023-05-03 10:36:55 +0600

    remove row groups

--- src/modules/settings/containers/ColorSettings/index.tsx
+++ src/modules/settings/containers/ColorSettings/index.tsx
@@ -69,101 +69,99 @@ const ColorSettings: FC = () => {
         </label>
       </RowGroup>
 
-      <Grid direction="row">
-        <RowGroup>
-          <label>
-            <SettingsRow title={t("Background")}>
-              <input
-                type="color"
-                onChange={setString("backgroundColor")}
-                value={settings.backgroundColor}
-              />
-            </SettingsRow>
-          </label>
+      <RowGroup>
+        <label>
+          <SettingsRow title={t("Background")}>
+            <input
+              type="color"
+              onChange={setString("backgroundColor")}
+              value={settings.backgroundColor}
+            />
+          </SettingsRow>
+        </label>
 
-          <label>
-            <SettingsRow title={t("Text")}>
-              <input
-                type="color"
-                onChange={setString("textColor")}
-                value={settings.textColor}
-              />
-            </SettingsRow>
-          </label>
+        <label>
+          <SettingsRow title={t("Text")}>
+            <input
+              type="color"
+              onChange={setString("textColor")}
+              value={settings.textColor}
+            />
+          </SettingsRow>
+        </label>
 
-          <label>
-            <SettingsRow title={t("Links")}>
-              <input
-                type="color"
-                onChange={setString("linkColor")}
-                value={settings.linkColor}
-              />
-            </SettingsRow>
-          </label>
+        <label>
+          <SettingsRow title={t("Links")}>
+            <input
+              type="color"
+              onChange={setString("linkColor")}
+              value={settings.linkColor}
+            />
+          </SettingsRow>
+        </label>
 
-          <label>
-            <SettingsRow title={t("Inline code")}>
-              <input
-                type="color"
-                onChange={setString("codeColor")}
-                value={settings.codeColor}
-              />
-            </SettingsRow>
-          </label>
-        </RowGroup>
+        <label>
+          <SettingsRow title={t("Inline code")}>
+            <input
+              type="color"
+              onChange={setString("codeColor")}
+              value={settings.codeColor}
+            />
+          </SettingsRow>
+        </label>
+      </RowGroup>
 
-        <RowGroup>
-          <label>
-            <SettingsRow title={t("Heading 1")}>
-              <input
-                type="color"
-                onChange={setString("h1Color")}
-                value={settings.h1Color || settings.textColor}
-              />
-            </SettingsRow>
-          </label>
+      <RowGroup>
+        <label>
+          <SettingsRow title={t("Heading 1")}>
+            <input
+              type="color"
+              onChange={setString("h1Color")}
+              value={settings.h1Color || settings.textColor}
+            />
+          </SettingsRow>
+        </label>
 
-          <label>
-            <SettingsRow title={t("Heading 2")}>
-              <input
-                type="color"
-                onChange={setString("h2Color")}
-                value={settings.h2Color || settings.textColor}
-              />
-            </SettingsRow>
-          </label>
+        <label>
+          <SettingsRow title={t("Heading 2")}>
+            <input
+              type="color"
+              onChange={setString("h2Color")}
+              value={settings.h2Color || settings.textColor}
+            />
+          </SettingsRow>
+        </label>
 
-          <label>
-            <SettingsRow title={t("Heading 3")}>
-              <input
-                type="color"
-                onChange={setString("h3Color")}
-                value={settings.h3Color || settings.textColor}
-              />
-            </SettingsRow>
-          </label>
+        <label>
+          <SettingsRow title={t("Heading 3")}>
+            <input
+              type="color"
+              onChange={setString("h3Color")}
+              value={settings.h3Color || settings.textColor}
+            />
+          </SettingsRow>
+        </label>
 
-          <label>
-            <SettingsRow title={t("Heading 4")}>
-              <input
-                type="color"
-                onChange={setString("h4Color")}
-                value={settings.h4Color || settings.textColor}
-              />
-            </SettingsRow>
-          </label>
+        <label>
+          <SettingsRow title={t("Heading 4")}>
+            <input
+              type="color"
+              onChange={setString("h4Color")}
+              value={settings.h4Color || settings.textColor}
+            />
+          </SettingsRow>
+        </label>
 
-          <label>
-            <SettingsRow title={t("Heading 5")}>
-              <input
-                type="color"
-                onChange={setString("h5Color")}
-                value={settings.h5Color || settings.textColor}
-              />
-            </SettingsRow>
-          </label>
-        </RowGroup>
-      </Grid>
+        <label>
+          <SettingsRow title={t("Heading 5")}>
+            <input
+              type="color"
+              onChange={setString("h5Color")}
+              value={settings.h5Color || settings.textColor}
+            />
+          </SettingsRow>
+        </label>
+      </RowGroup>
     </Grid>
   );
 };
